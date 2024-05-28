cc70766e42e5b4f94b83b4d15b1872d49f815c83
Stephboss9 <mbengastephane34@gmail.com>
2024-05-27 23:01:09 -0400

    added animations for an improved user experience

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ function scrollFunction() {
         navBar.classList.remove(".nav-links-border-active");
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const animatedParagraph = document.querySelector('.about-me-desc');
+    const animatedTitles = document.querySelectorAll('.animated-title');
+    const animatedWorkExperienceInfo = document.querySelectorAll('.animated-experience-info');
+    const projectAnimation = document.querySelectorAll('.project-animation');
+
+    const observerOptions = { threshold: 0.2 };
+    const observerCallBack = (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Stop observing once animation is triggered
+            }
+        });
+    };
+    observer = new IntersectionObserver(observerCallBack, observerOptions);
+    observer.observe(animatedParagraph);
+    animatedTitles.forEach(title => observer.observe(title));
+    animatedWorkExperienceInfo.forEach(experienceInfo => observer.observe(experienceInfo));
+    projectAnimation.forEach(project => observer.observe(project));
+});
